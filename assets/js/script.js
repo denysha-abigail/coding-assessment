05d16d64071fec795808138f0ea1e94a47ec3cc2
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-03-16 00:18:34 -0400

    added html and js code

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+var time = 5;
+var qAIndex = 0;
+
+var qA = [
+    {
+        question: 'Is the sky blue?',
+        answer: ["A", "B", "C", "D"],
+        correct: "C"
+    },
+    {
+        question: 'Is the sky blue?',
+        answer: ["A", "B", "C", "D"],
+        correct: "C"
+    },
+    {
+        question: 'Is the sky blue?',
+        answer: ["A", "B", "C", "D"],
+        correct: "C"
+    },
+    {
+        question: 'Is the sky blue?',
+        answer: ["A", "B", "C", "D"],
+        correct: "C"
+    }
+];
+
+function showQ() {
+    if (qAIndex < qA.length) {
+        let obj = qA[qAIndex];
+        document.querySelector('.container').innerHTML = `
+        <div>
+            <h2>${obj.question}</h2>
+        </div>
+        `
+    }
+}
+
+showQ();
+
+var intervalId = setInterval(startQuiz, 1000);
+
+function startQuiz() {
+    time--;
+    document.querySelector("#time").innerText = time;
+    if (time < 1) {
+        clearInterval(intervalId)
+    }
+}
